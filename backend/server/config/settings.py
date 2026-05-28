7605c749ab4015523895bcd1c84d3749364efb00
Antoine-Ringnyu <ntaryikeantoine@gmail.com>
2026-05-28 12:02:42 +0100

    fix: fix secret imports

--- backend/server/config/settings.py
+++ backend/server/config/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import Path
+from os import environ
+from pathlib import Path    
+
+# import environ
+env = environ.Env()
+environ.Env.read_env()  # read .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*mkowkgu7ga=&#(l$7#5_2x#y-bzcu(kxv-qbf@)=1*efja+=%'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
